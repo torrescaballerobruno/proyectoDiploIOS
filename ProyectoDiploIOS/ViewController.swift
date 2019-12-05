63c47ebff79d8726c7c99beab6eaa3d6858cdc85
Bruno Torres <bruno.tor.96@gmail.com>
2019-12-04 19:21:36 -0600

    Initial Commit

---
+++ ProyectoDiploIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoDiploIOS
+//
+//  Created by Bruno Torres on 04/12/19.
+//  Copyright © 2019 Bruno Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
